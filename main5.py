6688bca8eef44e94781eb7fd11d672eecd5c5ab0
Noah <107689370+NoahFilms@users.noreply.github.com>
2022-09-25 19:47:46 -0300

    Password-Manager main5
    add it main5

---
+++ main5.py
@@ -0,0 +1,62 @@
+def main():
+
+     try:
+        PasswordList = []
+        infile = open("ThePasswordList.txt", "r")
+        line = infile.readline()
+        while line:
+          PasswordList.append(line.strip("\n").split(",")
+          )
+          line = infile.readline()
+          infile.close
+
+     except FileNotFoundError:
+         print("the <passwordlist.txt> file is not found"
+         )
+         print("starting a new password list!")
+         PasswordList = []
+      
+     choice = 0
+     while choice !=4:
+      print("* Password Manager * ")
+      print("1) Add a Password")
+      print("2) Lookup a Password")
+      print("3) Display a Password")
+      print("4) Quit")
+      choice = int(input())
+
+      if choice == 1:
+        print("Addig a book...")
+        nWebsite = input("Enter the name of the Website>")
+        nEmail = input("Enter the name of the Email>")
+        nPassword = input("Enter the number of the Password>")
+        PasswordList.append([nWebsite, nEmail, nPassword])
+
+      elif choice == 2:
+          print("Looking up for a Password...")
+          keyword = input("Enter Search Term: ")
+          for book in PasswordList:
+            if keyword in book:
+                print(book)
+
+      elif choice == 3:
+          print("All Password...")
+          for i in range(len(PasswordList)):
+              print(PasswordList[i])
+
+
+
+
+      elif choice == 4:
+          print("Quitting Program")
+     print("Program Terminated!")
+
+     outfile = open("ThePasswordList.txt", "w")
+     for book in PasswordList:
+         outfile.write(",".join(book) + "\n")
+     outfile.close()
+        
+
+
+if __name__ == "__main__":
+    main()   
